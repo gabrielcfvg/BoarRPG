3efa25771498fe786fc8645bbdf6f6cdcae85c71
Leonardo <leonardodicredico@gmail.com>
2020-03-17 17:57:05 -0300

    Initial commit and launcher(c#)

---
+++ Desktop/Launcher/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BOARMMO
+{
+    public partial class Form1 : Form
+    {
+        Socket client = null;
+        private string ip = "127.0.0.1";
+        private int port = 6666;
+
+        public Form1()
+        {
+            InitializeComponent();
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.Connect(ip, port);
+        }
+
+        private void bLogin_Click(object sender, EventArgs e)
+        {
+            string user = textUser.Text;
+            string pass = textPass.Text;
+            string dt = protocolData(1, new String[] { user, pass, "login" });
+            sendData(dt);
+
+        }
+
+        private void bRegister_Click(object sender, EventArgs e)
+        {
+            string user = textUser.Text;
+            string pass = textPass.Text;
+            string dt = protocolData(1,new String[] { user, pass,"register" });
+            sendData(dt);
+        }
+
+        private string protocolData(int id,string[] values)
+        {
+            string output = id.ToString()+":";
+            foreach (var item in values)
+            {
+                output += item;
+                output += "|";
+            }
+            output = output.Remove(output.Length - 1);
+            return output;
+        }
+
+
+        private void sendData(string data)
+        {
+            byte[] protocoledData = Encoding.ASCII.GetBytes(data);
+            client.Send(protocoledData);
+        }
+
+        private void awaitAnswer()
+        {
+
+        }
+
+
+    }
+}
