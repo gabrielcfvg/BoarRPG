d2ca1853520962bf9bea05ee963f7542ae91da4b
gabrielcfvg <56398419+gabrielcfvg@users.noreply.github.com>
2020-04-09 21:34:06 -0300

    Terceirização de funções, sockets UDP e membros locais ao download de chunck

--- Server/Processamento/Server.py
+++ Server/Processamento/Server.py
@@ -1,9 +1,11 @@
 from threading import Thread
 import socket
-from server_funções import Usuário, Chunck
 from base64 import b64decode, b64encode
 from pickle import loads, dumps
 from traceback import print_exc
+
+from server_funções import Usuário, Chunck
+
 
 ##################################################################################################################
 #                                                                                                                #
@@ -12,9 +14,99 @@ from traceback import print_exc
 ##################################################################################################################
 
 
+class Funções:
+
+    @staticmethod
+    def thread_decorador(func):
+
+        def main(chave, pacote):
+
+            Thread(target=func, args=[chave, pacote], daemon=True).start()
+
+        return main
+
+    @staticmethod
+    def gerar_chave(chave_inicial):
+        from random import randint
+
+        chave = chave_inicial+str(randint(1, 9))
+
+        if chave in clients_ativos:
+            return Funções.gerar_chave(chave)
+        else:
+            return chave
+
+    @staticmethod
+    def login(chave, conexão):
+
+        from server_funções import login
+        from sqlite_crud import selecionar
+
+        autenticado = False
+        while not autenticado:
+
+            pacote = conexão.recv(2048).decode("utf-8")
+
+            if len(pacote) == 0:
+                Funções.pacote_nulo(chave, clients_ativos[chave]['endereço'], None)
+                raise Exception
+
+            print("pacote login:", pacote) 
+            resposta, _nome = login(pacote)
+            conexão.send(resposta)
+
+            if _nome != None:
+                nome = _nome
+                clients_ativos[chave]['nome'] = nome
+                autenticado = True
+
+                obj = loads(b64decode(selecionar(tabela='contas', v1='binário', v2='nome', v3=nome)))
+                clients_ativos[chave]['objeto'] = obj
+
+    @staticmethod
+    def remover_client(chave):
+        from sqlite_crud import atualizar
+
+        if not chave in clients_ativos:
+            return
+
+        if len(clients_ativos[chave]) == 4:
+
+            obj = b64encode(dumps(clients_ativos[chave]['objeto'])).decode('utf-8')
+            nome = clients_ativos[chave]['nome']
+
+            atualizar(tabela='contas', v1='binário', v2=obj, v3='nome', v4=nome)
+
+
+        del clients_ativos[chave]
+
+    @staticmethod
+    def pacote_nulo(chave, endereço, nome):
+
+        print(f"Usuário desconectado |{nome if nome else 'indefinido'}| {endereço}  motivo = Pacote Nulo")
+        try:
+            Funções.remover_client(chave)   
+        except:
+            Funções.remover_client(chave)
+
+    @staticmethod
+    def chunck_players(chave):
+
+        nome = clients_ativos[chave]['nome']
+        chunck = clients_ativos[chave]['objeto'].chunck
+
+        for A in clients_ativos:
+
+            if A in clients_ativos:
+
+                if clients_ativos[A]["nome"] != nome and clients_ativos[A]["objeto"].chunck == chunck:
+
+                    sock_UDP.sendto(bytes(f"1:{clients_ativos[A]['nome']}|{clients_ativos[A]['objeto'].localização}.", 'utf-8'), (clients_ativos[chave]['endereço'][0], 666))
+
 class Protocolos:
 
     @staticmethod
+    @Funções.thread_decorador
     def enviar_movimentação(chave, pacote):
 
         nome = clients_ativos[chave]['nome']
@@ -23,26 +115,28 @@ class Protocolos:
 
         for A in clients_ativos:
             
-            if clients_ativos[A]['nome'] != nome and clients_ativos[A]['conexão'].chunck == chunck:
+            if A in clients_ativos:
                 
-                clients_ativos[A]['conexão'].send(bytes(f"1:{nome}|{pacote}.", 'utf-8'))
+                if clients_ativos[A]['nome'] != nome and clients_ativos[A]['objeto'].chunck == chunck:
+
+                    #print('----------> ', clients_ativos[A]['nome'])
+
+                    sock_UDP.sendto(bytes(f"1:{nome}|{pacote}.", 'utf-8'), (clients_ativos[A]["endereço"][0], 666))
+
         
         clients_ativos[chave]['objeto'].localização = pacote
 
     @staticmethod
     def send_chunk(chave, pacote):
 
-
         from sqlite_crud import selecionar
         
 
         conexão = clients_ativos[chave]['conexão']
         nome = clients_ativos[chave]['nome']
-
         pacote = pacote.split(':')[1]
-        pacote = pacote.split(".")[0]
 
-        if (len(pacote.split('|')) == 1) and (pacote.split('|')[0] == '0'):
+        if (pacote.split('|')[0] == '0'):
             
             obj = loads(b64decode(selecionar(tabela='contas', v1='binário', v2='nome', v3=nome)))
             chunck_name = obj.chunck
@@ -113,70 +207,7 @@ class Protocolos:
             clients_ativos[chave]['objeto'].chunck = chunck_name
             conexão.send(bytes(saida, 'utf-8'))
 
-
-class Funções:
-
-    @staticmethod
-    def gerar_chave(chave_inicial):
-        from random import randint
-
-        chave = chave_inicial+str(randint(1, 9))
-
-        if chave in clients_ativos:
-            return Funções.gerar_chave(chave)
-        else:
-            return chave
-
-    @staticmethod
-    def login(chave, conexão):
-
-        from server_funções import login
-        from sqlite_crud import selecionar
-
-        autenticado = False
-        while not autenticado:
-
-            pacote = conexão.recv(2048).decode("utf-8")
-
-            if len(pacote) == 0:
-                Funções.pacote_nulo(chave, clients_ativos[chave]['endereço'], None)
-                raise Exception
-
-            print("pacote login:", pacote) 
-            resposta, _nome = login(pacote)
-            conexão.send(resposta)
-
-            if _nome != None:
-                nome = _nome
-                clients_ativos[chave]['nome'] = nome
-                autenticado = True
-
-                obj = loads(b64decode(selecionar(tabela='contas', v1='binário', v2='nome', v3=nome)))
-                clients_ativos[chave]['objeto'] = obj
-
-    @staticmethod
-    def remover_client(chave):
-        from sqlite_crud import atualizar
-
-
-        if len(clients_ativos[chave]) == 4:
-
-            obj = b64encode(dumps(clients_ativos[chave]['objeto'])).decode('utf-8')
-            nome = clients_ativos[chave]['nome']
-
-            atualizar(tabela='contas', v1='binário', v2=obj, v3='nome', v4=nome)
-
-
-        del clients_ativos[chave]
-
-    @staticmethod
-    def pacote_nulo(chave, endereço, nome):
-
-        print(f"Usuário desconectado |{nome if nome else 'indefinido'}| {endereço}  motivo = Pacote Nulo")
-        try:
-            Funções.remover_client(chave)   
-        except:
-            Funções.remover_client(chave)
+            Funções.chunck_players(chave)
 
 
 def receber(chave, conexão, endereço):
@@ -234,6 +265,8 @@ clients_ativos = {}
 IP = '0.0.0.0'
 PORTA = 1234
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 ##################################################################################################################
 #                                                                                                                #
@@ -241,7 +274,6 @@ PORTA = 1234
 #                                                                                                                #
 ##################################################################################################################
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((IP, PORTA))
 sock.listen()
 
@@ -254,6 +286,3 @@ while True:
     clients_ativos[chave] = {'conexão': conexão, 'endereço': endereço}
     
     Thread(target=receber, args=[chave, conexão, endereço], daemon=True).start()
-
-
-
